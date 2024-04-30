9806d1ac83a78936719a7ce3f7333150856b2e84
aaalliii <116755476+aaalliii@users.noreply.github.com>
2024-04-30 19:48:12 +0500

    Initial Commit

---
+++ idunno/idunnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  idunnoApp.swift
+//  idunno
+//
+//  Created by ali on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct idunnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
